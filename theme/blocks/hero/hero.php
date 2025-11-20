0d50a22a52e14c5a733fa2fe84cf7f67381fc75a
Alec Plescia <adples.design@gmail.com>
2025-11-20 11:03:09 -0500

    add loader back

--- theme/blocks/hero/hero.php
+++ theme/blocks/hero/hero.php
@@ -101,7 +101,7 @@ if ( get_field( 'bg' ) && ! $show_video ) {
 
 	<?php if ( $show_video ) : ?>
 
-			<?php // get_template_part( 'template-parts/components/loader' ); ?>
+			<?php get_template_part( 'template-parts/components/loader' ); ?>
 
 
 	<?php endif; ?>
@@ -110,14 +110,14 @@ if ( get_field( 'bg' ) && ! $show_video ) {
 
 <?php if ( $show_video ) : ?>
 <script>
-// function fadeOut(element, duration) {
-// 	element.style.transition = `opacity ${duration}ms`;
-// 	element.style.opacity = 0;
+function fadeOut(element, duration) {
+	element.style.transition = `opacity ${duration}ms`;
+	element.style.opacity = 0;
 
-// 	setTimeout(() => {
-// 		element.style.display = 'none';
-// 	}, duration);
-// }
+	setTimeout(() => {
+		element.style.display = 'none';
+	}, duration);
+}
 
 // const video = document.querySelector('.wp-block-acf-hero');
 // const loader = document.querySelector('.loader-overlay');
@@ -132,5 +132,26 @@ if ( get_field( 'bg' ) && ! $show_video ) {
 // 		}, 100);
 // 	});
 // }
+function onVimeoReady(callback) {
+	if (window.Vimeo && window.Vimeo.Player) {
+		callback();
+	} else {
+		setTimeout(() => onVimeoReady(callback), 50);
+	}
+}
+
+onVimeoReady(() => {
+	const video = document.querySelector('.wp-block-acf-hero');
+	const loader = document.querySelector('.loader-overlay');
+
+	if (video) {
+		const iframe = document.getElementById('vimeo');
+		const player = new Vimeo.Player(iframe);
+
+		player.on('play', () => {
+			setTimeout(() => fadeOut(loader, 1000), 100);
+		});
+	}
+});
 </script>
 <?php endif; ?>
